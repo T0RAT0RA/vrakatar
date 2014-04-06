d29a51d3433011376bc7b0b3578d9497663e3945
Stéphane Reiss <stephane.reiss@gmail.com>
2014-04-06 11:17:59 -0400

    Added real-time POC.

---
+++ stephane/vrakatar-world/game.js
@@ -0,0 +1,159 @@
+//Utils & constants
+var extend = require('util')._extend,
+    DIRECTION = {
+        "UP": 38,
+        "DOWN": 40,
+        "LEFT": 37,
+        "RIGHT": 39,
+    },
+    MAP = {WIDTH: 1000, HEIGHT: 400},
+    VELOCITY = {X: 10, Y:10};
+
+//Init game
+var Game = {},
+    players = {};
+
+Game.init = function() {
+};
+
+Game.update = function() {
+    //Update players pos
+    for (i in players) {
+        player = players[i];
+        player.position.x += player.velocity.x;
+        player.position.y += player.velocity.y;
+
+        //If player is not human (no socket associate)
+        //Move it randomly
+        if (!player.socket) {
+            player.position.x += Game.randomInt(-10, 10);
+            player.position.y += Game.randomInt(-10, 10);
+        }
+    }
+
+    Game.io.sockets.emit('game.state', Game.getState());
+};
+
+Game.stop = function() {
+    clearInterval(Game._intervalId);
+}
+
+Game.start = function(io) {
+    Game.io = io;
+    Game._intervalId = setInterval(Game.update, 100);
+
+    io.sockets.on('connection', function (socket) {
+        var player = {
+                id: Date.now(),
+                socket: socket,
+                name: "player" + (Object.keys(players).length+1),
+                position: {
+                    x: 0,
+                    y: 0,
+                },
+                velocity: {
+                    x: 0,
+                    y: 0,
+                },
+            };
+        players[player.id] = player;
+
+        socket.emit('game.init', {
+            map: MAP,
+            player: {
+                id: player.id
+            }
+        });
+
+        socket.on('player.name', function (data) {
+            players[player.id].name = data.name;
+        });
+
+        socket.on('player.keydown', function (direction) {
+            switch(direction) {
+                case DIRECTION["LEFT"]:
+                    player.velocity.y = -VELOCITY.Y;
+                break;
+                case DIRECTION["UP"]:
+                    player.velocity.x = -VELOCITY.X;
+                break;
+                case DIRECTION["RIGHT"]:
+                    player.velocity.y = VELOCITY.Y;
+                break;
+                case DIRECTION["DOWN"]:
+                    player.velocity.x = VELOCITY.X;
+                break;
+                default:
+            }
+        });
+        socket.on('player.keyup', function (direction) {
+            switch(direction) {
+                case DIRECTION["LEFT"]:
+                    player.velocity.y = 0;
+                break;
+                case DIRECTION["UP"]:
+                    player.velocity.x = 0;
+                break;
+                default:
+                case DIRECTION["RIGHT"]:
+                    player.velocity.y = 0;
+                break;
+                case DIRECTION["DOWN"]:
+                    player.velocity.x = 0;
+                break;
+            }
+        });
+
+        socket.on('game.addRandomPlayer', function () {
+            random_player = {
+                id: Date.now(),
+                socket: null,
+                name: "Random player" + (Object.keys(players).length+1),
+                position: {
+                    x: Game.randomInt(0, MAP.HEIGHT),
+                    y: Game.randomInt(0, MAP.WIDTH),
+                },
+                velocity: {
+                    x: 0,
+                    y: 0,
+                },
+            };
+            players[random_player.id] = random_player;
+        });
+
+        socket.on('disconnect', function (socket) {
+            delete players[player.id];
+            Game.io.sockets.emit('game.player.disconnect', player.id);
+        });
+    });
+}
+
+Game.getState = function() {
+    return {
+        status: Game._intervalId? "running" : "stopped",
+        players_count: Object.keys(players).length,
+        players: Game.getPlayers(),
+    }
+};
+
+Game.getPlayers = function(id) {
+    var data_players = [];
+
+    for (i in players) {
+        var player = extend({}, players[i]);
+        delete player.socket;
+
+        if (id == i) { return player; }
+        data_players.push(player);
+    }
+
+    return data_players;
+}
+
+Game.randomInt = function(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+module.exports = function(io) {
+    Game.start(io);
+};
---
+++ stephane/vrakatar-world/public/js/client.js
@@ -0,0 +1,87 @@
+function connect(name) {
+    var socket = io.connect('http://localhost');
+
+    $(".game").removeClass("not-connected").addClass("connected");
+    socket.on('disconnect', function(){
+        disconnect();
+        io.disconnect();
+    });
+
+    socket.emit('player.name', {name: name});
+    socket.on('game.init', function (data) {
+        $(".game").css({
+            width: data.map.WIDTH+"px",
+            height: data.map.HEIGHT+"px"
+        });
+    });
+
+    //Print game state:
+    var pre = $("<pre>").appendTo(".game-state");
+    $(".game-state").show();
+    socket.on('game.state', function (data) {
+        pre.html(JSON.stringify(data, null, 2));
+        //update players
+        for (i in data.players) {
+            player = data.players[i];
+            player.div = $(".player#"+player.id);
+
+            //Create the player div if not exist
+            if (!player.div.length) {
+                player.div = $("<div>").addClass("player").attr("id", player.id).appendTo(".game");
+                $("<div>").addClass("name")
+                    .html(player.name)
+                    .css({
+                        width: (player.name.length * 5)+"px",
+                        left: (-player.name.length * 5/2 + parseInt(player.div.css("width")) )+"px"
+                    })
+                    .appendTo(player.div);
+            }
+            player.div.css({
+                top: player.position.x,
+                left: player.position.y
+            });
+        }
+    });
+    socket.on('game.player.disconnect', function (playerId) {
+        $(".player#"+playerId).remove();
+    });
+
+
+    //Bind actions
+    $(window).on("keydown", function(e) {
+        socket.emit('player.keydown', e.keyCode);
+        e.preventDefault();
+    });
+
+    $(window).on("keyup", function(e) {
+        socket.emit('player.keyup', e.keyCode);
+        e.preventDefault();
+    });
+
+    $(".game-state").on("click", ".addRandomPlayer", function(e) {
+        socket.emit('game.addRandomPlayer');
+    });
+
+}
+
+function disconnect() {
+    $(".game").removeClass("connected").addClass("not-connected");
+    $(".player").remove();
+    $(".game-state").hide();
+    $(window).off("keyup keydown");
+}
+
+//Register player
+$(".game .register").on("submit", function(){
+    var name = $(this).find(".name");
+
+    if (!name.val()){
+        name.addClass("error");
+        name.focus();
+    } else {
+        name.removeClass("error");
+        connect(name.val());
+    }
+
+    return false;
+});

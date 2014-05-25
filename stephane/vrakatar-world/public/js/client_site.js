de6a11d5d8568b40a3fe786f1ed666ebb1a8acd3
Stéphane Reiss <stephane.reiss@gmail.com>
2014-05-24 23:11:13 -0400

    Removed old folder.

--- stephane/vrakatar-world/public/js/client_site.js
+++
@@ -1,5 +0,0 @@
-socket = io.connect('http://localhost:8002');
-
-socket.on('game.init', function (data) {
-    console.log(data);
-});
--- stephane/vrakatar-world/routes/index.js
+++
@@ -1,8 +0,0 @@
-/* GET home page. */
-exports.index = function(req, res){
-  res.render('index', { title: 'Vrakatar World' });
-};
-/* GET vrak page. */
-exports.vrak = function(req, res){
-  res.render('vrak', { title: 'Vrak' });
-};
